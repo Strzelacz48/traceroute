b0a509e36239c06c178103b278d72fa9d68080c3
Marcin Czapulak <73505362+Strzelacz48@users.noreply.github.com>
2025-03-24 12:53:56 +0100

    version merge

---
+++ marcin_czapulak/main.c
@@ -0,0 +1,42 @@
+#include "header.h"
+
+//324279 Marcin Czapulak
+
+int main(int argc, char *argv[]) {
+    //sprawdzanie ilości argumentów
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <IP addres >\n", argv[0]);//Nie uzywam ERROR bo argv[0], łatwiej zrobić wyjątek
+        return EXIT_FAILURE;
+    }
+    
+    struct sockaddr_in dest_addr;
+    memset(&dest_addr, 0, sizeof(dest_addr));//upewnienie się że nie będzie losowych danych
+    dest_addr.sin_family = AF_INET;
+    //sprawdzanie poprawności danych
+    if (inet_pton(AF_INET, argv[1], &dest_addr.sin_addr) != 1) ERROR("Incorrect IP addres");
+    //Przygotowanie socketa
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd < 0) ERROR("socket failed");
+    
+    struct timeval timeout = {TIMEOUT, 0};//sekunda na czekanie
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    int pid = getpid();
+    //główna pętla
+    struct sockaddr_in recv_addr;
+    for (int ttl = 1; ttl <= MAX_HOPS; ttl++) {
+        struct timeval start_time;
+        gettimeofday(&start_time, NULL);
+        
+        send3requests(sockfd, &dest_addr, ttl, pid);
+        receive_reply(sockfd, &recv_addr, &start_time, ttl);
+        
+        //jeśli doszliśmy do celu wyjdź z pętli
+        if (recv_addr.sin_addr.s_addr == dest_addr.sin_addr.s_addr) {
+            //printf("Destination reached\n");
+            break;
+        }
+    }
+    
+    close(sockfd);
+    return EXIT_SUCCESS;
+}
